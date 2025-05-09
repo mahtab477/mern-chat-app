f64d72c1519f82f544cad360269d52adb83d7ddb
Mahtab <mahtabali2k20@gmail.com>
2025-05-09 07:37:05 -0700

    first api create

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,25 @@
-const express = require("express")
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data.js");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("API is running");
+    res.send("API is running successfully");
+});
+
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
 })
 
-app.listen(5000, console.log("Server Started on PORT 5000"));
+app.get("/api/chat/:id", (req, res) => {
+    //console.log(req.params.id);
+    const singleChats = chats.find((c) => c._id === req.params.id);
+    res.send(singleChats);
+});
+
+const PORT = process.env.PORT || 5000; 
+
+app.listen(5000, console.log(`Server Started on PORT ${PORT}`));
 
