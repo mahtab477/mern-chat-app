9b6d9a4010ca983e78cd72444cdc1ccc4f1500ad
Mahtab <mahtabali2k20@gmail.com>
2025-05-17 08:05:20 -0700

    authUser encryptpassword

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -38,4 +38,43 @@ const registerUser = asyncHandler(async (req,res) => {
     }
 });
 
-module.exports = { registerUser };
+//Login
+const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await User.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            pic: user.pic,
+            token: generateToken(user._id),
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid Email or pasword");
+    }
+})
+
+//chat gpt suggestion
+// const registerUser = async (req, res) => {
+//     const { name, email, password } = req.body;
+
+//     try {
+//         const userExists = await User.findOne({ email });
+//         if (userExists) {
+//             return res.status(400).json({ message: "User already exists" });
+//         }
+
+//         const user = await User.create({ name, email, password });
+//         res.status(201).json(user);
+//     } catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// };
+
+
+
+module.exports = { registerUser , authUser };
